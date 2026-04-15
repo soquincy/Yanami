15ef5d94aca6b11417a515cf771bf539b56538be
Quincy So <books.quincyso@gmail.com>
2026-04-15 15:17:40 +0800

    Create status.py

---
+++ cogs/status.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands, tasks
+
+class StatusCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.update_status.start()
+
+    def cog_unload(self):
+        self.update_status.cancel()
+
+    @tasks.loop(minutes=5)
+    async def update_status(self):
+        await self.bot.wait_until_ready()
+        
+        if self.bot.guilds:
+            # Get the first server's name
+            server_name = self.bot.guilds[0].name
+            
+            # Set Presence: Watching [Server], Status: Do Not Disturb
+            # Options for status: .online, .idle, .dnd, .invisible
+            await self.bot.change_presence(
+                status=discord.Status.dnd, 
+                activity=discord.Activity(
+                    type=discord.ActivityType.watching, 
+                    name=server_name
+                )
+            )
+
+    @update_status.before_loop
+    async def before_update_status(self):
+        await self.bot.wait_until_ready()
+
+async def setup(bot):
+    await bot.add_cog(StatusCog(bot))
