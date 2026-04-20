1fb0c912d697d7c4e187fcb8151dca31f7de279f
Quincy So <books.quincyso@gmail.com>
2026-04-20 13:25:39 +0800

    Update status.py
    Multiple server status update

--- cogs/status.py
+++ cogs/status.py
@@ -4,6 +4,7 @@ from discord.ext import commands, tasks
 class StatusCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.index = 0
         self.update_status.start()
 
     async def cog_unload(self):
@@ -12,16 +13,27 @@ class StatusCog(commands.Cog):
     @tasks.loop(minutes=5)
     async def update_status(self):
         await self.bot.wait_until_ready()
+        
+        guilds = self.bot.guilds
+        if not guilds:
+            return
 
-        if self.bot.guilds:
-            server_name = self.bot.guilds[0].name
-            await self.bot.change_presence(
-                status=discord.Status.dnd,
-                activity=discord.Activity(
-                    type=discord.ActivityType.watching,
-                    name=server_name
-                )
+        # Ensure the index doesn't go out of bounds if the bot leaves a server
+        if self.index >= len(guilds):
+            self.index = 0
+
+        server_name = guilds[self.index].name
+        
+        await self.bot.change_presence(
+            status=discord.Status.dnd,
+            activity=discord.Activity(
+                type=discord.ActivityType.watching,
+                name=server_name
             )
+        )
+
+        # Move to the next server for the next 5-minute update
+        self.index += 1
 
     @update_status.before_loop
     async def before_update_status(self):
